0b8f3960422e285d3bf22bcf87736708c2075d07
d-beech <datubeechmusic@gmail.com>
2024-08-20 21:20:28 +1200

    Added Flask migration depencies to requirements.txt, added initial basic MenuItems model for testing

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 from flask import Flask, redirect, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/TedsBarAndCafeWebiste-DB'
+
+#Intitialize the database
+db = SQLAlchemy(app)
+migrate=Migrate(app, db)
+
+#Create db model
+class MenuItems(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    descrption = db.Column(db.String(200))
+    price = db.Column(db.Numeric, nullable=False)
+    
+    #Returns a string when we add an item
+    def __repr__(self):
+        return '<Name %r>' % self.id
+
+
+
+
+
 
 @app.route("/")
 def home():
