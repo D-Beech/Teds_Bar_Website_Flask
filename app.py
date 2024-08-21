5b1c04d6d4c8ed5476082c09a73195b7abbe8156
d-beech <datubeechmusic@gmail.com>
2024-08-21 18:03:14 +1200

    Added more code to additem

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -37,6 +37,29 @@ def menu():
 def order():
     return render_template("order.html")
 
+
+
+#This is a simple API I am using to learn flask and develop the item gallery/db, it will be removed
+@app.route("/addItem", methods=['POST','GET'])
+def addItem():
+
+    if request.method == "POST":
+        item_name = request.form['name']
+        item_description = request.form['description']
+        item_price = request.form['price']
+        new_item = MenuItems(name=item_name, descrption=item_description, price=item_price)
+
+        try:
+            db.session.add(new_item)
+            db.session.commit()
+            return redirect('/addItem')
+        except:
+            return "There was an error adding the item"
+            
+
+    else:
+        menu_items = MenuItems.query.all()
+        return render_template("addItem.html", stuff=menu_items)
 
 if __name__ == "__main__":
     app.run(debug=True)
