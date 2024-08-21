99f4c57d8ccd1f4c70d815c6ad5d7a3c3672311b
Rana123Mandeep <writetomandeeprana@gmail.com>
2024-08-22 10:51:46 +1200

    add a  image folder in static

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/TedsBarAndCafeWebiste-DB'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mandeepsingh@localhost:5432/TedsBarAndCafeWebiste-DB'
 
 #Intitialize the database
 db = SQLAlchemy(app)
